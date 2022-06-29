49234577b1067a9bcbe8aaf174b2ced4b334a5b7
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2022-06-29 14:13:00 +0200

    feat(HomeView.vue): created the navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!--Search / Results -->
+    <div
+      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-6 font-bold">
+          IP Address Tracker
+        </h1>
+        <div class="flex">
+          <input
+            type="text"
+            class="flex-1 py-3 px-2 rounder-tl-md rounder-bl-md focus:outline-none"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
+          <i
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md fas fa-chevron-right flex items-center"
+          ></i>
+        </div>
+      </div>
+    </div>
+    <!--Input-->
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('pattern-bg.png')",
+        // https://www.freepik.com/free-vector/topographic-map-contour-lines-water-colours_8848438.htm#page=3&query=map&position=9&from_view=search
+      }),
+    },
   },
   plugins: [],
-}
+};
